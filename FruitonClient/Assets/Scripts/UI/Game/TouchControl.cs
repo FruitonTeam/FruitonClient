d1f788acb24bcc4545a9ced1b9199684b4bef4de
Kerzak <lubomir.ohman@yahoo.com>
2017-04-08 12:59:48 +0200

    TouchControl refactoring.

--- FruitonClient/Assets/Scripts/UI/Game/TouchControl.cs
+++ FruitonClient/Assets/Scripts/UI/Game/TouchControl.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchControl : MonoBehaviour {
-    public GameObject board;
 
-    Dictionary<int, Touch> touches;
-    Vector3 translateDirection, translateNormal;
+    public GameObject board;
+    /// <summary> Y axis of the plane on which camera movec dring translation. </summary>
+    Vector3 translateDirection;
+    /// <summary> X axis of the plane on which camera movec dring translation. </summary>
+    Vector3 translateNormal;
 
     private void Start()
     {
-        touches = new Dictionary<int, Touch>();
         ComputeTranslateVectors();
     }
 
@@ -32,73 +33,68 @@ public class TouchControl : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// User performed an action using single <paramref name="touch"/>
+    /// </summary>
+    /// <param name="touch"></param>
     private void SingleTouchLogic(Touch touch)
     {
-        // TODO: collisions with fruitons
-        //Ray ray = Camera.main.ScreenPointToRay(touch.position);
-        //RaycastHit hit = new RaycastHit();
-        //if (Physics.Raycast(ray.origin, ray.direction, out hit, 1000))
-
-        Vector2 center = Camera.main.WorldToScreenPoint(board.transform.position);
-        float angle = FruitMath.GetAngleBetweenTwoPoints(touch.position - center, touch.position - touch.deltaPosition - center);
         // Rotations of the screen
         if (touch.phase == TouchPhase.Moved)
         {
+            // the center of the board
+            Vector2 center = Camera.main.WorldToScreenPoint(board.transform.position);
+            // angle between previous and current finger position
+            float angle = FruitMath.GetAngleBetweenTwoPoints(touch.position - center, touch.position - touch.deltaPosition - center);
             transform.RotateAround(new Vector3(0, 0, 0), Vector3.down, 2 * angle);
         }
 
     }
 
+    /// <summary>
+    /// Updates the values of translateDirection and translateNormal.
+    /// </summary>
     private void ComputeTranslateVectors()
     {
         Plane planeXZ = new Plane(Vector3.up, Vector3.zero);
         Vector3 center;
-        FruitMath.LinePlaneIntersection(out center, Camera.main.transform.position, Camera.main.transform.forward, Vector3.up, Vector3.zero);
-        
+        FruitMath.LinePlaneIntersection(out center, Camera.main.transform.position, Camera.main.transform.forward, Vector3.up, Vector3.zero); 
         center.y = 0;
         Vector3 camera = Camera.main.transform.position;
-        camera.y = 0;
+        // Project the camera location to XZ plane.
+        camera.y = 0; 
         translateDirection = (center - camera).normalized;
         translateNormal = new Vector3(translateDirection.z, 0, -translateDirection.x);
     }
 
+    /// <summary>
+    /// User performed an action using two touches.
+    /// </summary>
     private void DoubleTouchLogic(Touch touch1, Touch touch2)
     {
         if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
         {
+            // Comptute the translation vectors in case translation will occur.
             ComputeTranslateVectors();
-
         }
+        // Translation
         else if ((touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Stationary) ||
             (touch2.phase == TouchPhase.Moved && touch1.phase == TouchPhase.Stationary))
         {
-            Vector3 delta = (new Vector3(touch1.deltaPosition.x, 0, touch1.deltaPosition.y) + new Vector3(touch2.deltaPosition.x, 0, touch2.deltaPosition.y)) / 2;
+            // Move the camera on a plane that is parallerl to the plane given by board and intrsects with the camera coords.
+            Vector3 delta = new Vector3(touch1.deltaPosition.x, 0, touch1.deltaPosition.y) + new Vector3(touch2.deltaPosition.x, 0, touch2.deltaPosition.y);
             Camera.main.transform.position -= delta.x * translateNormal + delta.z * translateDirection;
         }
+        // Scale (pinch scale)
         else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
         {
-
-            //if (touch1.phase == TouchPhase.Moved && touch2.phase == TouchPhase.Moved && FruitMath.GetAngleBetweenTwoPoints(touch1.deltaPosition, touch2.deltaPosition) < 45)
-            //{
-
-            //}
-            //else
-            {
-                float distance = Vector2.Distance(touch1.position, touch2.position);
-                float lastDistance = Vector2.Distance(touch1.position - touch1.deltaPosition, touch2.position - touch2.deltaPosition);
-                float zoom;
-                zoom = 0.1f * (lastDistance - distance);
-                Camera.main.fieldOfView += zoom;
-                float fieldOfView = Camera.main.fieldOfView;
-                //if (fieldOfView < 15)
-                //{
-                //    Camera.main.fieldOfView = 15;
-                //}
-                //if (fieldOfView > 150)
-                //{
-                //    Camera.main.fieldOfView = 150;
-                //}
-            }
+            float distance = Vector2.Distance(touch1.position, touch2.position);
+            float lastDistance = Vector2.Distance(touch1.position - touch1.deltaPosition, touch2.position - touch2.deltaPosition);
+            float zoom;
+            // Scale according to the delta of finger distances. 
+            zoom = 0.1f * (lastDistance - distance);
+            Camera.main.fieldOfView += zoom;
+            float fieldOfView = Camera.main.fieldOfView;
         }
     }
 }
