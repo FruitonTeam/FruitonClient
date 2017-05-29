aa81e8576e0364f3131174496d482f0e21e81334
Jozef Lelic <joseph.lelic@gmail.com>
2017-05-29 11:59:07 +0200

    Connect haxe to unity - Update build script to compile haxe kernel - Add temporary test scene for testing kernel in unity - Add test script that uses generated code - Update .gitignore to ignore generated code - Update readme

---
+++ FruitonClient/Assets/Scripts/Kernel/KernelTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KernelTest : MonoBehaviour {
+
+    public Button yourButton;
+
+    Random random;
+    GameState gameState;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(GenerateNewMove);
+
+        random = new Random();
+
+        gameState = Kernel.generateState();
+        Debug.Log(gameState.pieces);
+    }
+
+    void GenerateNewMove()
+    {
+        var moveId = (int)Random.Range(0, 5);
+        var moveX = (int)Random.Range(-5, 5);
+        var moveY = (int)Random.Range(-5, 5);
+        Move move = new Move(moveId, new Position(moveX, moveY));
+        Debug.Log(move.ToString());
+        gameState = Kernel.nextState(gameState, move);
+        Debug.Log(gameState.pieces);
+    }
+}
