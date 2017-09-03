38de91391637b35b98d9c6b023a52e7fe6a73ce6
Kerzak <lubomir.ohman@yahoo.com>
2017-09-03 17:13:51 +0200

    Haxe wierdness explanation comment added.

--- FruitonClient/Assets/Scripts/Fruitons/ClientFruitonFactory.cs
+++ FruitonClient/Assets/Scripts/Fruitons/ClientFruitonFactory.cs
@@ -14,6 +14,7 @@ public static class ClientFruitonFactory {
         System.Collections.Generic.List<ClientFruiton> result = new List<ClientFruiton>();
         foreach (int key in fruitonDatabase.fruitonDb._keys)
         {
+            // This check is here because Haxe IntMap may also contain invalid key-value pairs, which can be recognised by the fact that their key is 0. 
             if (key == 0) continue;
             Fruiton kernelFruiton = FruitonFactory.makeFruiton(key, fruitonDatabase);
             result.Add(new ClientFruiton(kernelFruiton));
