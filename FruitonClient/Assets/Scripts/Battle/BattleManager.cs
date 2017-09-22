ce7708a9994658d7f9ea24a396ef30dbc28584b5
Kerzak <lubomir.ohman@yahoo.com>
2017-09-22 10:10:39 +0200

    Demo loading of opponent team.

--- FruitonClient/Assets/Scripts/Battle/BattleManager.cs
+++ FruitonClient/Assets/Scripts/Battle/BattleManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,42 +14,51 @@ public class BattleManager : MonoBehaviour {
     {
         gridLayoutManager = GridLayoutManager.Instance;
         gameManager = GameManager.Instance;
-        grid = new ClientFruiton[gridLayoutManager.HeighCount, gridLayoutManager.WidthCount];
+        grid = new ClientFruiton[gridLayoutManager.WidthCount, gridLayoutManager.HeighCount];
         IEnumerable<ClientFruiton> currentTeam = ClientFruitonFactory.CreateClientFruitonTeam(gameManager.CurrentFruitonTeam.FruitonIDs);
+        // TODO: This is just temporary offline solution. It is needed to obtain opponent team from server.
+        IEnumerable<ClientFruiton> opponentTeam = ClientFruitonFactory.CreateClientFruitonTeam(gameManager.CurrentFruitonTeam.FruitonIDs);
+        InitializeTeam(currentTeam, true);
+        InitializeTeam(opponentTeam, false);
+    }
 
+    private void InitializeTeam(IEnumerable<ClientFruiton> currentTeam, bool down)
+    {
+        int majorRow = down ? 0 : gridLayoutManager.HeighCount - 1;
+        int minorRow = down ? 1 : majorRow - 1;
         int majorCounter = 2;
         int minorCounter = 2;
         int i = 0, j = 0;
         foreach (ClientFruiton clientFruiton in currentTeam)
         {
             clientFruiton.FruitonObject.AddComponent<BoxCollider>();
-            
+
             switch (clientFruiton.KernelFruiton.type)
             {
                 case 1:
-                {
+                    {
                         i = gridLayoutManager.WidthCount / 2;
-                        j = 0;
-                } break;
+                        j = majorRow;
+                    }
+                    break;
                 case 2:
-                {
+                    {
                         i = gridLayoutManager.WidthCount / 2 - majorCounter;
-                        j = 0;
+                        j = majorRow;
                         if (--majorCounter == 0) --majorCounter;
-                }
-                break;
+                    }
+                    break;
                 case 3:
-                {
+                    {
                         i = gridLayoutManager.WidthCount / 2 - minorCounter;
-                        j = 1;
+                        j = minorRow;
                         --minorCounter;
-                }
-                break;
+                    }
+                    break;
             }
             grid[i, j] = clientFruiton;
             Vector3 cellPosition = gridLayoutManager.GetCellPosition(i, j);
             clientFruiton.FruitonObject.transform.position = cellPosition + new Vector3(0, clientFruiton.FruitonObject.transform.lossyScale.y, 0);
-
         }
     }
 }
