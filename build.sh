ec06afd6112f9952f161c019827bb2cc0f977860
Pavel Šmejkal <smejkal.pa@gmail.com>
2017-10-27 12:08:29 +0200

    Remove unnecessary env variables
    Only two are needed now: `KERNEL_LOC` and `PROTO_LOC`. The reset is
    inferred and assumes that the scirpt is ran from its directory.
    Line endings for `*.sh` files set to lf to work fine on Linux subsystem.

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 
 # Kernel
 echo "Compiling kernel..."
-KERNEL_DEST=$FRUITON_CLIENT_LOC/FruitonClient/Assets/Scripts/Kernel/Generated
+
+FRUITON_CLIENT_LOC=`pwd`
+KERNEL_DEST=${FRUITON_CLIENT_LOC}/FruitonClient/Assets/Scripts/Kernel/Generated
 mkdir -p ${KERNEL_DEST}
 
-UNITY_RESOURCES_LOC=$FRUITON_CLIENT_LOC/FruitonClient/Assets/Resources
+UNITY_RESOURCES_LOC=${FRUITON_CLIENT_LOC}/FruitonClient/Assets/Resources
 mkdir -p ${UNITY_RESOURCES_LOC}
 
 cd ${KERNEL_LOC}
@@ -13,10 +15,13 @@ cd ${KERNEL_LOC}
 haxe --macro include\(\'fruiton\'\,true\,\[\'fruiton.fruitDb.models\'\,\'fruiton.macro\'\]\) -D no-compilation -D no-root -cs ${KERNEL_DEST} -cp ${KERNEL_LOC} fruiton.kernel.Kernel
 
 cp -r ${KERNEL_LOC}/resources/* ${UNITY_RESOURCES_LOC}
+cd ${FRUITON_CLIENT_LOC}
 
 
 # Protobufs
 echo "Compiling protobufs..."
+
+PROTO_DEST="FruitonClient/Assets/Scripts/Protobufs"
 mkdir -p ${PROTO_DEST}
 
 for proto_file in ${PROTO_LOC}/*.proto; do
