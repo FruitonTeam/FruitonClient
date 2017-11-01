40edcf612ab5f38b2f4f199327025fc8793ef98d
Pavel Šmejkal <smejkal.pa@gmail.com>
2017-11-01 17:48:37 +0100

    Add Spine and animated fruitons to team management

---
+++ FruitonClient/Assets/Scripts/SpineAnimatedGameObjects/Fruitons/Pea/PeaTMAnimator.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Spine.Unity;
+
+public class PeaTMAnimator : MonoBehaviour
+{
+    public CharaterSide characterSide;
+
+    private SkeletonAnimation skeletonAnim;
+
+    public float AnimSwitchTime;
+    private float currentAnimTime;
+
+    // 0 - drawOrderBack/Side/(Front)                                           // decide which side to turn + change skins FrontQuarter/BackQuarter
+    // 1 - blackEyeOff                                                          // no black eye
+    // 2 - blackEyeRight/Left                                                   // turn on one of them
+    // 3 - topOff/fallTop                                                       // toggle top of the body
+    // 4 - starRingOff/starRingTwirl                                            // after taking damage star ring appears around its head
+    // 5 - startWalk/walk/scratchMustache/wideStandGetGuns/fire/takeDamage      // usual animations
+
+    private static readonly int USUAL_T = 5;
+
+    private static readonly System.Random rnd = new System.Random();
+
+    private enum IdleAnimations
+    {
+        Walk,
+        Stand,
+        Fire,
+        TakeDamage
+    }
+
+    private IdleAnimations currentIdleAnimation;
+
+    private void FinishCurrentAnim()
+    {
+        skeletonAnim.AnimationState.SetEmptyAnimation(USUAL_T, 0);
+    }
+
+    private void AddNextRandomAnim()
+    {
+        var values = Enum
+            .GetValues(typeof(IdleAnimations))
+            .Cast<IdleAnimations>()
+            .Where(x => x != currentIdleAnimation)
+            .ToList();
+
+        currentIdleAnimation = values[rnd.Next(values.Count)];
+
+        switch (currentIdleAnimation)
+        {
+            case IdleAnimations.Fire:
+                Attack();
+                break;
+            case IdleAnimations.Stand:
+                StandIdle();
+                break;
+            case IdleAnimations.TakeDamage:
+                TakeDamageIdle();
+                break;
+            case IdleAnimations.Walk:
+                StartWalking();
+                break;
+        }
+    }
+
+    private void StartWalking()
+    {
+        Debug.Log("Doing StartWalking");
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "startWalk", false, 0);
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "walk", true, 0);
+    }
+
+    private void TakeDamageIdle()
+    {
+        Debug.Log("Doing TakeDamageIdle");
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "takeDamage", false, 0);
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "takeDamage", false, 3);
+    }
+
+    private void StandIdle()
+    {
+        Debug.Log("Doing StandIdle");
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "scratchMustache", false, 0);
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "scratchMustache", false, 4);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Doing Attack");
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "wideStandGetGuns", false, 0);
+        skeletonAnim.AnimationState.AddAnimation(USUAL_T, "fire", false, 0);
+        skeletonAnim.AnimationState.AddEmptyAnimation(USUAL_T, 0, 0);
+        // TODO Move weapons back so they do not float
+    }
+
+    private void TurnCharacter(CharaterSide side)
+    {
+        switch (side)
+        {
+            case CharaterSide.FRONT_L:
+                skeletonAnim.AnimationState.ClearTrack(0);
+                skeletonAnim.skeleton.SetSkin("FrontQuarter");
+                skeletonAnim.skeleton.FlipX = false;
+                skeletonAnim.skeleton.SetSlotsToSetupPose();
+                skeletonAnim.AnimationState.Apply(skeletonAnim.Skeleton);
+                break;
+            case CharaterSide.FRONT_R:
+                skeletonAnim.AnimationState.ClearTrack(0);
+                skeletonAnim.skeleton.SetSkin("FrontQuarter");
+                skeletonAnim.skeleton.FlipX = true;
+                skeletonAnim.skeleton.SetSlotsToSetupPose();
+                skeletonAnim.AnimationState.Apply(skeletonAnim.Skeleton);
+                break;
+            //case CharaterSide.SIDE_R:
+            //    skeletonAnim.skeleton.FlipX = true;
+            //    break;
+            case CharaterSide.BACK_R:
+                skeletonAnim.skeleton.FlipX = true;
+                skeletonAnim.AnimationState.SetAnimation(0, "drawOrderBack", false);
+                skeletonAnim.skeleton.SetSkin("BackQuarter");
+                skeletonAnim.skeleton.SetSlotsToSetupPose();
+                skeletonAnim.AnimationState.Apply(skeletonAnim.Skeleton);
+                break;
+            case CharaterSide.BACK_L:
+                skeletonAnim.skeleton.FlipX = false;
+                skeletonAnim.AnimationState.SetAnimation(0, "drawOrderBack", false);
+                skeletonAnim.skeleton.SetSkin("BackQuarter");
+                skeletonAnim.skeleton.SetSlotsToSetupPose();
+                skeletonAnim.AnimationState.Apply(skeletonAnim.Skeleton);
+                break;
+            //case CharaterSide.SIDE_L:
+            //    skeletonAnim.skeleton.FlipX = false;
+            //    break;
+        }
+        characterSide = side;
+
+    }
+
+    private void ResetCharacter()
+    {
+        skeletonAnim.AnimationState.SetAnimation(1, "blackEyeOff", true);
+        skeletonAnim.AnimationState.ClearTrack(2);
+        skeletonAnim.AnimationState.ClearTrack(3);
+        skeletonAnim.AnimationState.SetAnimation(4, "starRingOff", true);
+        skeletonAnim.AnimationState.ClearTrack(USUAL_T);
+    }
+
+    private void StartTeamManagementIdle()
+    {
+        StartWalking();
+        currentIdleAnimation = IdleAnimations.Walk;
+    }
+
+    private void Awake()
+    {
+        skeletonAnim = GetComponent<SkeletonAnimation>();
+    }
+
+    private void Start()
+    {
+        ResetCharacter();
+        StartTeamManagementIdle();
+    }
+
+    private void Update()
+    {
+        currentAnimTime += Time.deltaTime;
+        if (currentAnimTime > AnimSwitchTime)
+        {
+            Debug.Log("Changing anim");
+            currentAnimTime -= AnimSwitchTime;
+            FinishCurrentAnim();
+            AddNextRandomAnim();
+        }
+    }
+}
