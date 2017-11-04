7c78fc46ddbe9308fc11565011ce81e66b768b8e
Pavel Šmejkal <smejkal.pa@gmail.com>
2017-11-04 17:18:01 +0100

    Remove excessive output from bat scripts

--- clean.bat
+++ clean.bat
@@ -1,12 +1,13 @@
-echo "Cleaning..."
+@echo off
+echo Cleaning...
 
 :: Remove develop directories
-del /s /q FruitonClient\Assets\Scripts\Kernel
-del /s /q FruitonClient\Assets\Scripts\Protobufs
+del /s /q FruitonClient\Assets\Scripts\Kernel >nul 2>&1
+del /s /q FruitonClient\Assets\Scripts\Protobufs >nul 2>&1
 
 :: Remove changeable libraries
-del /q FruitonClient\Assets\Libraries\protobufs.dll
-del /q FruitonClient\Assets\Libraries\kernel.dll
+del /q FruitonClient\Assets\Libraries\protobufs.dll >nul 2>&1
+del /q FruitonClient\Assets\Libraries\kernel.dll >nul 2>&1
 
 :: Remove resources
-del /s /q FruitonClient\Assets\Resources\Kernel
+del /s /q FruitonClient\Assets\Resources\Kernel >nul 2>&1
