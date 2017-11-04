35779cf38c69982ac853e851c1cee14b8020cbbd
Adam Hornáček <adam.hornacek@icloud.com>
2017-11-04 13:47:27 +0100

    SWP-162 download kernel and protobufs from artifactory
    Initial commit

--- FruitonClient/Assets/Scripts/GameManager.cs
+++ FruitonClient/Assets/Scripts/GameManager.cs
@@ -1,22 +1,19 @@
 ﻿using Cz.Cuni.Mff.Fruiton.Dto;
 using fruiton.fruitDb;
-using fruiton.fruitDb.factories;
-using fruiton.kernel;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Xml.Serialization;
 using Networking;
 using UnityEngine;
+using Util;
 using KFruiton = fruiton.kernel.Fruiton;
 
 public enum FractionNames { None, GuacamoleGuerrillas, CranberryCrusade, TzatzikiTsardom }
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour 
+{    
     public static GameManager Instance { get; private set; }
 
+    private static readonly string FRUITON_DB_FILE = "FruitonDb.json";
+    
     #region Fields
 
     private string userName;
@@ -100,7 +97,7 @@ public class GameManager : MonoBehaviour {
     {
         get
         {
-            return (PlayerPrefs.GetInt("ValidUser", 0) == 1)? true: false;
+            return PlayerPrefs.GetInt("ValidUser", 0) == 1;
         }
         set
         {
@@ -176,7 +173,8 @@ public class GameManager : MonoBehaviour {
     public void Initialize()
     {
         Serializer.DeserializeFruitonTeams();
-        FruitonDatabase = new FruitonDatabase(Resources.Load<TextAsset>("FruitonDb").text);
+                
+        FruitonDatabase = new FruitonDatabase(KernelUtils.LoadTextResource(FRUITON_DB_FILE));
         AllFruitons = ClientFruitonFactory.CreateAllKernelFruitons();
         AvailableFruitons = Serializer.LoadAvailableFruitons();
 
@@ -185,27 +183,6 @@ public class GameManager : MonoBehaviour {
             fruitonTeamList = ints;
         },
         Debug.Log);
-    }
-
-    #endregion
-
-    #region Private
-
-    private void SerializeBinary(object toBeSerialized, string filename)
-    {
-        FileStream stream = File.Create(filename);
-        var formatter = new BinaryFormatter();
-        formatter.Serialize(stream, toBeSerialized);
-        stream.Close();
-    }
-
-    private  T DeserializeBinary<T>(string filename)
-    {
-        FileStream stream = File.OpenRead(filename);
-        var formatter = new BinaryFormatter();
-        T deserialized = (T)formatter.Deserialize(stream);
-        stream.Close();
-        return deserialized;
     }
 
     #endregion
--- build.sh
+++ build.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# Kernel
-echo "Compiling kernel..."
-
-FRUITON_CLIENT_LOC=`pwd`
-KERNEL_DEST=${FRUITON_CLIENT_LOC}/FruitonClient/Assets/Scripts/Kernel/Generated
-mkdir -p ${KERNEL_DEST}
-
-UNITY_RESOURCES_LOC=${FRUITON_CLIENT_LOC}/FruitonClient/Assets/Resources
-mkdir -p ${UNITY_RESOURCES_LOC}
-
-cd ${KERNEL_LOC}
-
-haxe --macro include\(\'fruiton\'\,true\,\[\'fruiton.fruitDb.models\'\,\'fruiton.macro\'\]\) -D no-compilation -D no-root -cs ${KERNEL_DEST} -cp ${KERNEL_LOC} fruiton.kernel.Kernel
-
-cp -r ${KERNEL_LOC}/resources/* ${UNITY_RESOURCES_LOC}
-cd ${FRUITON_CLIENT_LOC}
-
+. pom.properties
 
 # Protobufs
-echo "Compiling protobufs..."
+echo "Downloading protobufs library..."
+rm protobufs-${protobufsVersion}.dll 2> /dev/null
 
-PROTO_DEST="FruitonClient/Assets/Scripts/Protobufs"
-mkdir -p ${PROTO_DEST}
+wget --header="User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0" http://prak.mff.cuni.cz:8081/artifactory/libs-release/protobufs-${protobufsVersion}.dll
 
-for proto_file in ${PROTO_LOC}/*.proto; do
-    protoc --csharp_out=${PROTO_DEST} --proto_path=${PROTO_LOC} ${proto_file}
-done
+mv protobufs-${protobufsVersion}.dll FruitonClient/Assets/Libraries/protobufs.dll
+
+# Kernel
+echo "Downloading kernel library..."
+rm kernel-${kernelVersion}.dll 2> /dev/null
+
+wget --header="User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:23.0) Gecko/20100101 Firefox/23.0" http://prak.mff.cuni.cz:8081/artifactory/libs-release/kernel-${kernelVersion}.dll
+
+mv kernel-${kernelVersion}.dll FruitonClient/Assets/Libraries/kernel.dll
+
