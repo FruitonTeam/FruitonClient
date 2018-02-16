49330e37a239d7af96e854216ce52cde0386049a
Jozef Lelic <joseph.lelic@gmail.com>
2018-02-16 14:28:58 +0100

    SWP-273 Add initial scene script

---
+++ FruitonClient/Assets/Scripts/InitialScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitialScene : MonoBehaviour {
+
+	void Start () {
+		SceneManager.LoadScene(Scenes.LOGIN_SCENE);
+	}
+}
