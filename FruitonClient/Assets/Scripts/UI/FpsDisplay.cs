bfb63e2d1ea768a7aa0ac2e04298187b74a410f9
Jozef Lelic <joseph.lelic@gmail.com>
2018-03-01 14:26:15 +0100

    SWP-475 Optimize performance on Android devices - reduce size of several sprites - limit maximum size of several textures in android build - remove terrain from battle scene - remove lights from battle scene, use unlit shaders - rescale texts in main menu - add fps display to initial scene (deactivated)

---
+++ FruitonClient/Assets/Scripts/UI/FpsDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsDisplay : MonoBehaviour
+{
+    public float RefreshTime = 0.5f;
+
+    private Text text;
+    private int frameCounter = 0;
+    private float timeCounter = 0.0f;
+    private float lastFramerate = 0.0f;
+
+    void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	void Update () {
+	    if (timeCounter < RefreshTime)
+	    {
+	        timeCounter += Time.deltaTime;
+	        frameCounter++;
+	    }
+	    else
+	    {
+	        lastFramerate = (float)frameCounter / timeCounter;
+	        frameCounter = 0;
+	        timeCounter = 0.0f;
+	    }
+
+	    text.text = (Mathf.Round(lastFramerate * 10) / 10f).ToString();
+	}
+}
